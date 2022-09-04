92c86738def313f29ff4985e3ce1fed85a0959ad
User <User@IVANNUZHYN>
2022-09-04 12:57:52 +0200

    Add project files.

---
+++ TODO_list/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TODO_list;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TODO_list/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TODO_list;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
